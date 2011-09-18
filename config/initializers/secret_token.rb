0e8f6b040a2a35ad1bd4a7d7986e800b653cc084
Samnang Chhun <samnang.chhun@gmail.com>
2011-09-18 22:07:08 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxRails31Demo::Application.config.secret_token = 'a728b9a95525de0e1427337cb82c09fbe451d29df2c723b94f0ab7f6d73977fa9be1d94cfb075be1ff5a4db5085d5815e6d1821e17c02c4fb860fe5622259af9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AjaxRails31Demo::Application.config.session_store :cookie_store, key: '_ajax_rails31_demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AjaxRails31Demo::Application.config.session_store :active_record_store
